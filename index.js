bf0d5db678e7a39aad5a3988087fd7c8b67d0369
shlapnya <shlapnya@instinctools.com>
2021-05-27 18:06:52 +0300

    Use config from options

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-videojs.registerPlugin('annotoDev', function () {
-    var player = this;
-    var config = {
-        widgets: [
-            {
-                player: {
-                    type: 'videojs',
-                    element: player.el()
-                }
-            }
-        ],
-        demoMode: true
+videojs.registerPlugin('annotoDev', function (options) {
+    var brightcovePlayer = this;
+    var defaultPlayer = {
+        type: 'videojs',
+        element: brightcovePlayer.el()
     };
+
+    var pluginConfig = options.config || {};
+    var widgets = pluginConfig.widgets || [];
+    var widget = widgets[0] || {};
+    var player = widget.player || {};
+    widget.player = Object.assign(player, defaultPlayer);
+
+    var config = Object.assign(pluginConfig, { widgets: [ widget ] });
 
     window.Annoto.boot(config);
 });
